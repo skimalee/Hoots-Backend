cbbe6ac9815a3d8535d2c50eb35e0b456ec9c676
Ashley Sands <ashley.sands01@gmail.com>
2026-01-15 08:27:38 +1300

    add post route for hoots

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const logger = require('morgan');
 const testJwtRouter = require('./controllers/test-jwt');
 const authRouter = require('./controllers/auth');
 const userRouter = require('./controllers/users');
+const hootsRouter = require('./controllers/hoots.js')
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -23,6 +24,7 @@ app.use(logger('dev'));
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
 app.use('/test-jwt', testJwtRouter);
+app.use('/hoots', hootsRouter)
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
